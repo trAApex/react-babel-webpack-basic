999620aa1cb1c0387ad44ee213efed3eb2935abe
Beer Adam <beer.adam92@gmail.com>
2018-11-03 00:27:12 +0100

    I practiced, how to use React with Babel and Webpack

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port, () => 
+    console.log(`Server started on port ${port}`)
+);
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: __dirname + '/public/js',
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [ 'style-loader', 'css-loader' ]
+            }
+        ]
+    }
+}
